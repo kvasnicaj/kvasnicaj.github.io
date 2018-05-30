862bcdfcc15f0b5054445528d8b44c528fb38b6c
Jaroslav Kvasnica <j.kvasnica@gmail.com>
2018-05-30 13:44:36 +0200

    mongodb collection to pandas dataframe

--- twitter/import-to-mongo.py
+++ twitter/import-to-mongo.py
@@ -3,6 +3,7 @@ import twitter statuses to Mongodb, oauth setting is in config.py
 '''
 from pymongo import MongoClient
 from twitter import OAuth, Twitter
+import pandas as pd
 from config import ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET
 
 oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
@@ -19,3 +20,7 @@ db = client.test
 # insert statuses to collection (1 status = 1 document)
 for line in tweets['statuses']:
     db.tweets.insert(line)
+
+# save mongodb collection to dataframe
+df = pd.DataFrame(list(db.tweets.find()))
+print(pd.info())
